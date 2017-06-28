048795de3d0384e8f7e0ad8d5eba63f99b55badc
Sam Street <samstreet@null.net>
2017-06-28 08:53:20 +0100

    Minor tweaks

--- Trackmate/Core/ServiceRegister.php
+++ Trackmate/Core/ServiceRegister.php
@@ -5,8 +5,52 @@
 
 namespace Trackmate\Core;
 
+use Trackmate\Interfaces\IServiceLocatorAware;
 
-class ServiceRegister
+class ServiceRegister implements IServiceLocatorAware
 {
+	/**
+	 * The register of services
+	 * @var
+	 */
+	protected $register;
+	
+	public function __construct()
+	{
+		$this->register = new Collection();
+	}
+	
+	/**
+	 * Register a new service with the collection
+	 * @param $identifier
+	 * @param $service
+	 * @return Collection
+	 */
+	public function register($identifier, $service, $params= []){
+		$this->register->add($identifier, $service);
+		
+		return $this;
+	}
+	
+	/**
+	 * Get a servcice
+	 * @param $identifier
+	 * @return mixed
+	 */
+	public function get($identifier)
+	{
+		// TODO: Implement get() method.
+	}
+	
+	/**
+	 * Do we have a service
+	 * @param $identifier
+	 * @return mixed
+	 */
+	public function has($identifier)
+	{
+		return $this->register->has($identifier);
+	}
+	
 	
 }
